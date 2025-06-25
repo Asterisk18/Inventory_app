a23533f3fd8aa5018cf4a51560865c4ca49401f1
asp117920 <asit1872006@gmail.com>
2025-06-25 15:42:32 +0530

    final changes

--- app/decorators.py
+++ app/decorators.py
@@ -6,6 +6,6 @@ def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not current_user.is_authenticated or not current_user.is_admin:
-            abort(403)  # Forbidden access
+            abort(403)
         return f(*args, **kwargs)
     return decorated_function
